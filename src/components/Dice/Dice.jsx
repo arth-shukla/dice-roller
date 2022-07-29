495882ebf8c7cd114c1d7b5007aae222d254727e
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-07-29 01:18:11 -0700

    more work to make 'blocks'

--- src/components/Dice/Dice.jsx
+++ src/components/Dice/Dice.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Dice.css'
-import { Alert, Button, } from 'react-bootstrap';
+import { Alert, Button, Stack, InputGroup, Form, ButtonGroup } from 'react-bootstrap';
 import DiceImage from '../../assets/DiceImage'
 
 
@@ -8,14 +8,30 @@ function Dice(props) {
     const variant = 'info'
     const [result, setResult] = useState('')
     const [rolls, setRolls] = useState('')
-    return <div style={{width:'100%'}} {...props}>
-        <DiceImage dice={props.dice} size={"50px"} />
-        {/* {Array(5).fill(0).map((x, i) => {
-            return <Button className="rollButton" type="primary">{i+1}</Button>
-        })} */}
-        <Alert key={variant} variant={variant}>
-            {rolls}
-        </Alert>
+    return <div style={{width:'467px'}} {...props}>
+        <Stack className="roll-controls" direction="horizontal" gap={2}>
+            <DiceImage dice={props.dice} />
+            <ButtonGroup>
+                {Array(8).fill(0).map((x, i) => {
+                    return <Button size="sm" className="roll-button" variant="outline-primary">{i+1+props.dice}</Button>
+                })}
+            </ButtonGroup>
+        </Stack>
+        <Stack direction="horizontal" gap={3}>
+            <Alert key={variant} variant={variant} style={{ width:"100%", margin: "auto" }}>
+                {rolls}
+            </Alert>
+            <Stack className="ms-auto" direction="horizontal" gap={2}>
+                <InputGroup size="sm" className="custom-roll-val">
+                    <InputGroup.Text>Custom</InputGroup.Text>
+                    <Form.Control
+                        placeholder="#"
+                        aria-label={`Enter a custom number of ${props.dice} to roll.`}
+                    />
+                </InputGroup>
+                <Button size="sm" className="roll-button-custom" type="primary">Roll</Button>
+            </Stack>
+        </Stack>
     </div>
 }
 
