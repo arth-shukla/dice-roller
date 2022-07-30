cd52c8325238858ddeac3b52a238b3956665e8c4
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-07-29 22:55:35 -0700

    Updates

--- src/components/Cell/Cell.jsx
+++
@@ -1,9 +0,0 @@
-import "./Cell.scss"
-
-function Cell({children, className, ...rest}) {
-    return <div className={(className || '') + ' cell'} {...rest}>
-        {children}
-    </div>
-}
-
-export default Cell
