017105c94ebf0b97af8c92a01c8582da69be85e9
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-08-01 00:18:17 -0700

    Update ThemeSwitches.jsx

--- src/components/ThemeSwitches/ThemeSwitches.jsx
+++ src/components/ThemeSwitches/ThemeSwitches.jsx
@@ -4,9 +4,7 @@ function LightDarkSwitch({ setThemeMode, isMoon, ...rest }) {
     return <button 
         id={isMoon ? "moon-dark-switch" : "sun-dark-switch"}
         class="dark-switch"
-        onClick={() => {
-            setThemeMode()
-        }}
+        onClick={() => { setThemeMode() }}
         {...rest}
     >
         <div id='targ'></div>
@@ -18,9 +16,7 @@ function LightColorSwitch({ setThemeMode, isDefault, ...rest }) {
     return <button 
         id={isDefault ? "default-color-switch" : "pink-color-switch"} 
         class="dark-switch" 
-        onClick={() => {
-            setThemeMode()
-        }}
+        onClick={() => { setThemeMode() }}
         {...rest}
     >
         <div id='targ'></div>
